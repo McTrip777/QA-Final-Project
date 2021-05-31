ab0237378f0869ba21d8879de697a77a4d51f571
McTrip777 <jacob.m.mcfaul@gmail.com>
2021-05-30 19:28:43 -0600

    Added Item class to domain and added daoItem class to Dao - Continued on to create read all

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,31 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    FOREIGN KEY (`customer_id`) REFERENCES customers(`id`), 
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `orders_items`;
+
+CREATE TABLE IF NOT EXISTS `orders_items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `orders_id` INT(11) NOT NULL,
+    `items_id` INT(11) NOT NULL,
+    FOREIGN KEY (`orders_id`) REFERENCES orders(`id`),
+    FOREIGN KEY (`items_id`) REFERENCES items(`id`), 
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
